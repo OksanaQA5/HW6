00f0023031e2998241e1799469f119b652d43a64
Oksana <cymbaloksana2003@gmail.com>
2023-06-28 16:13:30 +0300

    Initial commit

---
+++ src/Primitive.java
@@ -0,0 +1,7 @@
+public class Primitive {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Text for output");
+        int a = 7;
+        int b = 9;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(b - a);
+        System.out.println(a * b);
+        System.out.println(b / a);
+        System.out.println(b % a);
+
+
+    }
+        }
