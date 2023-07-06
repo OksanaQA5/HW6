9d6d408804857f243f89ae4e17415326dd9230db
Oksana <cymbaloksana2003@gmail.com>
2023-07-06 13:06:13 +0300

    Update HW#3

--- src/HW3/StringMetods3.java
+++ src/HW3/StringMetods3.java
@@ -1,38 +1,34 @@
 package HW3;
 
-public class StringMetods3 {
-            public static void main(String[] args) {
-                String text = "Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method.";
+public class StringMethods3 {
+    public static void main(String[] args) {
+        String text = "Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method.";
 
-                // Разбиваем строку на массив слов
-                String[] words = text.split("\\s+");
+        String[] words = text.split("\\s+");
 
-                int countLowerCaseA = 0;
-                int countUpperCaseA = 0;
+        int countLowerCaseA = 0;
+        int countUpperCaseA = 0;
 
-                // Проходим по каждому слову
-                for (String word : words) {
-                    // Приводим слово и символ 'a' к нижнему регистру для сравнения
-                    String lowerCaseWord = word.toLowerCase();
-                    char lowerCaseA = 'a';
+        for (String word : words) {
 
-                    // Считаем количество символов 'a' в слове с учетом регистра
-                    for (char c : lowerCaseWord.toCharArray()) {
-                        if (c == lowerCaseA) {
-                            countLowerCaseA++;
-                        }
-                    }
+            String lowerCaseWord = word.toLowerCase();
+            char lowerCaseA = 'a';
 
-                    // Считаем количество символов 'A' в слове с учетом регистра
-                    for (char c : word.toCharArray()) {
-                        if (c == 'A') {
-                            countUpperCaseA++;
-                        }
-                    }
+            for (char c : lowerCaseWord.toCharArray()) {
+                if (c == lowerCaseA) {
+                    countLowerCaseA++;
                 }
-
-                System.out.println("Количество символов 'a' в тексте (с учетом регистра): " + countLowerCaseA);
-                System.out.println("Количество символов 'A' в тексте (с учетом регистра): " + countUpperCaseA);
-                System.out.println("Общее количество символов 'a' и 'A' в тексте: " + (countLowerCaseA + countUpperCaseA));
             }
+
+            for (char c : word.toCharArray()) {
+                if (c == 'A') {
+                    countUpperCaseA++;
+                }
+            }
+        }
+
+        System.out.println("Количество символов 'a' в тексте (с учетом регистра): " + countLowerCaseA);
+        System.out.println("Количество символов 'A' в тексте (с учетом регистра): " + countUpperCaseA);
+        System.out.println("Общее количество символов 'a' и 'A' в тексте: " + (countLowerCaseA + countUpperCaseA));
+    }
 }
