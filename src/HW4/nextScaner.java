218c4eecaf3f36cf7e391a4e4ebeaaf9f0a8eb6f
Oksana <cymbaloksana2003@gmail.com>
2023-07-06 12:12:21 +0300

    Update HW#1

--- src/HW4/nextScaner.java
+++ src/HW4/nextScaner.java
@@ -1,24 +1,31 @@
 package HW4;
-public class Main {
+
+import java.util.Scanner;
+
+public class nextScaner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Я тестирую замечательно. Что еще нужно?");
+        String input = scanner.next();
+
         System.out.print("Я");
-        String string1 = scanner.next();
-
+        String input1 = scanner.next();
         System.out.print("тестирую");
-        String string2 = scanner.next();
-
-        System.out.print("замечательно");
-        String string3 = scanner.next();
-
+        String input2 = scanner.next();
+        System.out.print("замечательно.");
+        String input3 = scanner.next();
         System.out.print("Что еще нужно?");
-        String string4 = scanner.next();
-        string4 += scanner.nextLine(); // Добавляем остаток строки
+        String input4 = scanner.next();
 
-        System.out.println("string1: " + string1);
-        System.out.println("string2: " + string2);
-        System.out.println("string3: " + string3);
-        System.out.println("string4: " + string4);
+        String string1 = input;
+        String string2 = scanner.next();
+        String string3 = scanner.next();
+        String string4 = scanner.next();
+        System.out.println("string1 = " + string1);
+        System.out.println("string2 = " + string2);
+        System.out.println("string3 = " + string3);
+        System.out.println("string4 = " + string4);
+        scanner.close();
     }
 }
