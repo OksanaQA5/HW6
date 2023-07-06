19ee88ebd4bc1797b699440fa11e8f24283510ec
Oksana <cymbaloksana2003@gmail.com>
2023-07-06 18:21:16 +0300

    Update HW#4

--- src/HW4/nextScaner.java
+++ src/HW4/nextScaner.java
@@ -9,23 +9,14 @@ public class nextScaner {
         System.out.print("Я тестирую замечательно. Что еще нужно?");
         String input = scanner.next();
 
-        System.out.print("Я");
-        String input1 = scanner.next();
-        System.out.print("тестирую");
-        String input2 = scanner.next();
-        System.out.print("замечательно.");
-        String input3 = scanner.next();
-        System.out.print("Что еще нужно?");
-        String input4 = scanner.next();
-
         String string1 = input;
         String string2 = scanner.next();
         String string3 = scanner.next();
-        String string4 = scanner.next();
+        String string4 = scanner.next() + scanner.nextLine();
+
         System.out.println("string1 = " + string1);
         System.out.println("string2 = " + string2);
         System.out.println("string3 = " + string3);
         System.out.println("string4 = " + string4);
-        scanner.close();
     }
 }
