3d78cacf04caf7d311d0b4e0e607f79031457925
Oksana <cymbaloksana2003@gmail.com>
2023-07-06 12:46:14 +0300

    Update HW#3

--- src/HW3/StringMetods2.java
+++ src/HW3/StringMetods2.java
@@ -3,25 +3,25 @@ package HW3;
 public class StringMetods2 {
         public static void main(String[] args) {
             String string = "Testing, is my favourite job";
-            String[] words = string.split("\\s+");// Разбиваем строку на отдельные слова
+            String word1 = string.split("\\s+")[0];
+            word1 = word1.replace(",", "");
+            String word2 = string.split("\\s")[1];
+            String word3 = string.split("\\s")[2];
+            String word4 = string.split("\\s")[3];
+            String word5 = string.split("\\s")[4];
+            System.out.println("Cлово1 = " + word1 + ", Длина этого слова = " + word1.length() + ".");
+            System.out.println("Cлово2 = " + word2 + ", Длина этого слова = " + word2.length() + ".");
+            System.out.println("Cлово3 = " + word3 + ", Длина этого слова = " + word3.length() + ".");
+            System.out.println("Cлово4 = " + word4 + ", Длина этого слова = " + word4.length() + ".");
+            System.out.println("Cлово5 = " + word5 + ", Длина этого слова = " + word5.length() + ".");
 
-            // Выводим каждое слово и его длину
-            for (int i = 0; i < words.length;i++) {
-                String word = words[i];
-                int length = word.length();
-                System.out.println("Слово" + (i) + " = " + word + ", Длина этого слова = " + length + ".");
-            }
-
-            // Проверяем, является ли первое слово длиннее остальных
-            boolean isFirstWordLonger = true;
-            for (int i = 1; i < words.length; i++) {
-                if (words[i].length() > words[0].length()) {
-                    isFirstWordLonger = false;
-                    break;
-                }
-            }
-
-            System.out.println(isFirstWordLonger);
+            boolean result = word1.length() > word2.length() &&
+                    word1.length() > word3.length() &&
+                    word1.length() > word4.length() &&
+                    word1.length() > word5.length();
+            System.out.println(result);
         }
-    }
+
+
+}
 
