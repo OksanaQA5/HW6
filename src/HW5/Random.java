8c140ed936f4e913638db0e9f032a8f241e9f670
OksanaQA5 <137555719+OksanaQA5@users.noreply.github.com>
2023-07-11 19:21:58 +0300

    Update Random.java

--- src/HW5/Random.java
+++ src/HW5/Random.java
@@ -6,25 +6,21 @@ import java.util.random.RandomGenerator;
 
 public class Random {
     public static void main(String[] args) {
-        // Создаем массив и заполняем его случайными числами
-        int[] array = new int[10];
+           int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100); // Генерируем случайное число от 0 до 99
         }
 
-        // Выводим массив на экран
-        System.out.println("Массив:");
+       System.out.println("Массив:");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        // Считываем число от пользователя
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
         int targetNumber = scanner.nextInt();
 
-        // Проверяем наличие числа в массиве
         boolean found = false;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == targetNumber) {
@@ -32,8 +28,7 @@ public class Random {
                 break;
             }
         }
-
-        // Выводим результат
+        
         if (found) {
             System.out.println("Число " + targetNumber + " найдено в массиве.");
         } else {
